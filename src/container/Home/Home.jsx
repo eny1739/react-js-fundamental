9ab3c6116a391dab6cbff41621834bb1ea1fb93e
eny <61767395+eny1739@users.noreply.github.com>
2022-04-30 10:47:40 +0700

    create dinamic component with props

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -8,9 +8,18 @@ class Home extends Component {
       <div>
         <p>YouTube Component</p>
         <hr />
-        <YoutubeComp/>
-        <YoutubeComp/>
-        <YoutubeComp/>
+        <YoutubeComp
+          time="7.12"
+          title="Cara Membuat Sate"
+          desc="10x ditonton, 2 hari yang lalu"/>
+        <YoutubeComp
+          time="8.34"
+          title="Cara Membuat Boneka"
+          desc="200x ditonton, 10 hari yang lalu"/>
+        <YoutubeComp
+          time="5.14"
+          title="Cara Mengubah Tampilan PC"
+          desc="500x ditonton, 14 hari yang lalu"/>
         <YoutubeComp/>
       </div>
     );
